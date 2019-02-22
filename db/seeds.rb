eb39857cb6c094ec3b7bccafcbc07be40c7fe2fe
David Bordeleau <davidbordeleau@hotmail.com>
2019-02-22 11:17:45 +0100

    Add root

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Cocktail.create(name: "Death in the Afternoon")
+Cocktail.create(name: "Green Vesper")
+Cocktail.create(name: "Moloko Plus")
+
